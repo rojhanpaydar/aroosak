63e3c0a03f40738a4773e16305c628edf7d74952
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-03-12 21:09:26 -0500

    added new features to website, not mobile friendly yet.

---
+++ curator.js
@@ -0,0 +1,13 @@
+/* curator-feed-default-feed-layout */
+(function () {
+  var i,
+    e,
+    d = document,
+    s = "script";
+  i = d.createElement("script");
+  i.async = 1;
+  i.src =
+    "https://cdn.curator.io/published/c91e38e4-01dd-4dea-a931-bdf710943cb9.js";
+  e = d.getElementsByTagName(s)[0];
+  e.parentNode.insertBefore(i, e);
+})();
